05b9cbd767e2cf60faa48cda206b36cf8f04925d
Šimon Vacek <simonvacky@email.cz>
2022-03-05 16:03:47 +0100

    generating xml output done

--- parse.php
+++ parse.php
@@ -1,6 +1,7 @@
 <?php
 require("./utils.php");
 
+ini_set('display_errors', 'stderr');
 handleScriptArgs($argc, $argv);
 parse();
 
@@ -62,58 +63,79 @@ function checkNOArgs(string $instruction, array $args): bool
 /**
  * Kontroluje typ neterminalu <symb>.
  * @param $arg string Argument dane instrukce.
+ * @param $xmlWrt XMLWriter Objekt, s generovanym XML dokumentem.
  * @return bool Zda je argument typem neterminalu <symb>.
  */
-function isSymbOk(string $arg): bool
+function isSymbOk(string $arg, XMLWriter $xmlWrt): bool
 {
     // symbol muze byt i promenna
-    if(isVarOk($arg))
+    if(isVarOk($arg, $xmlWrt))
         return true;
 
     if(str_starts_with($arg, "int@")) {
+        $xmlWrt->writeAttribute("type", "int");
+        $xmlWrt->text(preg_replace("/^int@/", "", $arg));
         return preg_match("/^int@-?[0-9]+$/", $arg) == 1;
     }
     elseif(str_starts_with($arg, "string@")) {
+        $xmlWrt->writeAttribute("type", "string");
+        $xmlWrt->text(preg_replace("/^string@/", "", $arg));
         return preg_match("/^string@([^\s#\\\]|\\\[0-9][0-9][0-9])*$/", $arg) == 1;
     }
     elseif(str_starts_with($arg, "bool@")) {
+        $xmlWrt->writeAttribute("type", "bool");
+        $xmlWrt->text(preg_replace("/^bool@/", "", $arg));
         return (strcmp("bool@true", $arg) == 0 || strcmp("bool@false", $arg) == 0);
     }
-    else
+    else{
+        $xmlWrt->writeAttribute("type", "int");
+        $xmlWrt->text("nil");
         return strcmp("nil@nil", $arg) == 0;
+    }
 }
 
 /**
  * Kontroluje typ neterminalu <var>.
  * @param $arg string Argument dane instrukce.
+ * @param $xmlWrt XMLWriter Objekt, s generovanym XML dokumentem.
  * @return bool Zda je argument typem neterminalu <var>.
  */
-function isVarOk(string $arg): bool
+function isVarOk(string $arg, XMLWriter $xmlWrt): bool
 {
     if(preg_match("/^(GF@|TF@|LF@)/", $arg) == 0)
         return false;
+
+    $xmlWrt->writeAttribute("type", "var");
+    $xmlWrt->text($arg);
+
     $id = preg_replace("/^(GF@|TF@|LF@)/", "", $arg);
     // cast za oznacenim ramce je odpovida syntaxi labelu
-    return isLabelOk($id);
+    return preg_match("/^[_\-$&%*!?]?[a-zA-Z0-9]+$/", $id) == 1;
 }
 
 /**
  * Kontroluje typ neterminalu <type>.
  * @param $arg string Argument dane instrukce.
+ * @param $xmlWrt XMLWriter Objekt, s generovanym XML dokumentem.
  * @return bool Zda je argument typem neterminalu <type>.
  */
-function isTypeOk(string $arg): bool
+function isTypeOk(string $arg, XMLWriter $xmlWrt): bool
 {
+    $xmlWrt->writeAttribute("type", "type");
+    $xmlWrt->text($arg);
     return strcmp("int", $arg) == 0 || strcmp("string", $arg) == 0 || strcmp("bool", $arg) == 0;
 }
 
 /**
  * Kontroluje typ neterminalu <label>.
  * @param $arg string Argument dane instrukce.
+ * @param $xmlWrt XMLWriter Objekt, s generovanym XML dokumentem.
  * @return bool Zda je argument typem neterminalu <label>
  */
-function isLabelOk(string $arg): bool
+function isLabelOk(string $arg, XMLWriter $xmlWrt): bool
 {
+    $xmlWrt->writeAttribute("type", "label");
+    $xmlWrt->text($arg);
     return preg_match("/^[_\-$&%*!?]?[a-zA-Z0-9]+$/", $arg) == 1;
 }
 
@@ -121,9 +143,10 @@ function isLabelOk(string $arg): bool
  * Zkontroluje zda typy parametru odpovidaji instukci.
  * @param $instruction string Nazev instrukce
  * @param $args string[] Pole s parametry instrikce
+ * @param $xmlWrt XMLWriter Objekt, s generovanym XML dokumentem.
  * @return bool Zda sedi typy argumentu instrukce.
  */
-function checkParseArgs(string $instruction, array $args): bool
+function checkParseArgs(string $instruction, array $args, XMLWriter $xmlWrt): bool
 {
     global $instrArguments;
     $argTypes = $instrArguments[$instruction];
@@ -133,46 +156,47 @@ function checkParseArgs(string $instruction, array $args): bool
         return true;
     }
     // bude pocitat na jakem parametru se nachazim
-    $counter = 0;
+    $counter = 1;
 
     // podle counteru se sdruzi instrukce, ktere maji stejny typ parametru na stejne pozici
     while(sizeof($args) != 0) {
+        $xmlWrt->startElement("arg{$counter}");
         switch ($counter) {
-            case 0:
+            case 1:
                 switch($argTypes) {
                     case ArgType::Symb:
-                        if(!isSymbOk($args[0]))
+                        if(!isSymbOk($args[0], $xmlWrt))
                             return false;
                         break;
 
                     case ArgType::Label:
                     case ArgType::LabelSymbSymb:
-                        if(!isLabelOk($args[0]))
+                        if(!isLabelOk($args[0], $xmlWrt))
                             return false;
                         break;
 
                     default:
-                        if(!isVarOk($args[0]))
+                        if(!isVarOk($args[0], $xmlWrt))
                             return false;
                         break;
                 }
                 break;
 
-            case 1:
+            case 2:
                 if($argTypes == ArgType::VarType) {
-                    if (!isTypeOk($args[0]))
+                    if (!isTypeOk($args[0], $xmlWrt))
                         return false;
                 }
                 else
-                    if(!isSymbOk($args[0]))
+                    if(!isSymbOk($args[0], $xmlWrt))
                         return false;
                     break;
 
-            case 2:
+            case 3:
                 if($argTypes != ArgType::VarSymbSymb || $argTypes != ArgType::LabelSymbSymb)
                     return false;
                 else {
-                    if(!isSymbOk($args[0]))
+                    if(!isSymbOk($args[0], $xmlWrt))
                         return false;
                 }
                 break;
@@ -180,6 +204,7 @@ function checkParseArgs(string $instruction, array $args): bool
         // posunu se na dalsi parametr tim, ze odeberu z pole ten prvni
         $counter++;
         array_shift($args);
+        $xmlWrt->endElement();
     }
     return true;
 }
@@ -195,6 +220,15 @@ function parse() {
         exit(ERR_HEADER);
     }
 
+    $xmlWrt = new XMLWriter();
+    $xmlWrt->openMemory();
+    $xmlWrt->startDocument("1.0", "UTF-8");
+    $xmlWrt->startElement("program");
+    $xmlWrt->writeAttribute("language", "IPPcode22");
+    $xmlWrt->setIndent(true);
+    $xmlWrt->setIndentString("    ");
+
+    $order = 1;
     while(($line = fgets(STDIN)) != false) {
         // rozdelim radek na op kod a parametry
         $code = getCode($line);
@@ -206,23 +240,35 @@ function parse() {
         $code[0] = strtolower($code[0]);
         if(!isInstr($code[0])) {
             fprintf(STDERR,"Neznámý operační kód: %s\n", $code[0]);
+            $xmlWrt->flush();
             exit(ERR_OPP_CODE);
         }
 
-        // kontrola poctu argumentu instrukce
         $instruction = array_shift($code);
+        $xmlWrt->startElement("instruction");
+        $xmlWrt->writeAttribute("order", $order);
+        $xmlWrt->writeAttribute("opcode", strtoupper($instruction));
+
+        // kontrola poctu argumentu instrukce
         if(checkNOArgs($instruction, $code) == false) {
             fprintf(STDERR, "Špatný počet argumentů instrukce: %s\n", $instruction);
+            $xmlWrt->flush();
             exit(ERR_LEX_SYN);
         }
 
         // kontrola typu argumentu instrukce
-        if(!checkParseArgs($instruction, $code)) {
+        if(!checkParseArgs($instruction, $code, $xmlWrt)) {
             fprintf(STDERR, "Špatný typ argumentů instrukce: %s\n", $instruction);
+            $xmlWrt->flush();
             exit(ERR_LEX_SYN);
         }
-
+        $xmlWrt->endElement();
+        $order++;
     }
+
+    $xmlWrt->endElement();
+    $xmlWrt->endDocument();
+    echo $xmlWrt->outputMemory();
 }
 
 ?>
